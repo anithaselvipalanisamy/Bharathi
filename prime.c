246969c042c40f7679a9a27dfa3b561363bff281
anithaselvipalanisamy <31499326+anithaselvipalanisamy@users.noreply.github.com>
2018-01-18 21:25:49 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -6,11 +6,11 @@ printf("enter the number");
 scanf("%d",&a);
 if(a%2==0)
 {
-printf("the number is prime%d",a);
+printf("the number is  not a prime number%d",a);
 }
 else
 {
-printf("the number is not prime%d",a);
+printf("the number is a prime number%d",a);
 }
 return 0;
 }
