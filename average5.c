a18e4eafca19b821f5ba566cd58e55e1914e3674
anithaselvipalanisamy <31499326+anithaselvipalanisamy@users.noreply.github.com>
2018-01-24 21:37:27 +0530

    Update average5.c

--- average5.c
+++ average5.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int b,i;
+int b,i,s=0;
 scanf("%d",&b);
 int a[b];
-for(i=1;i<b;i++)
+for(i=1;i<=b;i++)
 {
 scanf("%d",&a[i]);
-printf("%d",a[i]);
+printf("%d ",a[i]);
+  s=s+a[i];
+}
+s=s/b;
+  printf("the average value is %d",s);
+return 0;
 }
