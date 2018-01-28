e477025f1d44081253b6698c2d3613d6cfb09801
anithaselvipalanisamy <31499326+anithaselvipalanisamy@users.noreply.github.com>
2018-01-28 16:58:05 +0530

    Create one.c

---
+++ one.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+switch(a)
+{
+case 1:printf("\none");
+      break;
+ case 2:printf("\ntwo");
+       break;
+ case 3:printf("\nthree");
+       break;
+ case 4:printf("\nfour");
+       break;
+case 5:printf("\nfive");
+break;
+case 6:printf("\nsix");
+break;
+case 7:printf("\nseven");
+break;
+case 8:printf("\neight");
+break;
+case 9:printf("\bnnine");
+break;
+case 10:printf("\nten");
+break;
+default:printf("\nit not exsist");
+}
+return 0;
+}
